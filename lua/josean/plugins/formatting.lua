d2104880dff67ba37a5c0389ed7eb7f459ca4eb8
AndrewDuc <andrewduc2722@gmail.com>
2024-10-09 16:50:06 +0000

    add live grep

--- lua/josean/plugins/formatting.lua
+++ lua/josean/plugins/formatting.lua
@@ -2,33 +2,32 @@ return {
   "stevearc/conform.nvim",
   event = { "BufReadPre", "BufNewFile" },
   config = function()
-    local conform = require("conform")
+	local conform = require("conform")
 
-    conform.setup({
-      formatters_by_ft = {
-        javascript = { "prettier" },
-        typescript = { "prettier" },
-        javascriptreact = { "prettier" },
-        typescriptreact = { "prettier" },
-        svelte = { "prettier" },
-        css = { "prettier" },
-        html = { "prettier" },
-        json = { "prettier" },
-        yaml = { "prettier" },
-        markdown = { "prettier" },
-        graphql = { "prettier" },
-        liquid = { "prettier" },
-        lua = { "stylua" }
-      },
-      format_on_save = false, -- Tắt tính năng format khi lưu file
-    })
+	conform.setup({
+		formatters_by_ft = {
+			javascript = { "prettier" },
+			typescript = { "prettier" },
+			javascriptreact = { "prettier" },
+			typescriptreact = { "prettier" },
+			svelte = { "prettier" },
+			css = { "prettier" },
+			html = { "prettier" },
+			json = { "prettier" },
+			yaml = { "prettier" },
+			markdown = { "prettier" },
+			graphql = { "prettier" },
+			liquid = { "prettier" },
+			lua = { "stylua" },
+		},
+		format_on_save = false,	})
 
-    vim.keymap.set({ "n", "v" }, "<leader>mp", function()
-      conform.format({
-        lsp_fallback = true,
-        async = false,
-        timeout_ms = 1000,
-      })
-    end, { desc = "Format file or range (in visual mode)" })
+	vim.keymap.set({ "n", "v" }, "<leader>mp", function()
+		conform.format({
+			lsp_fallback = true,
+			async = false,
+			timeout_ms = 1000,
+		})
+	end, { desc = "Format file or range (in visual mode)" })
   end,
 }
