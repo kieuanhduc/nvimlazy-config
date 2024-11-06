6f7fd98be74d60e7a0672bdb0d16856ede855024
AndrewDuc <andrewduc2722@gmail.com>
2024-11-06 14:51:37 +0000

    update telescope

--- lua/josean/plugins/telescope.lua
+++ lua/josean/plugins/telescope.lua
@@ -27,14 +27,20 @@ return {
         path_display = { "smart" },
         mappings = {
           i = {
-            ["<C-k>"] = actions.move_selection_previous, -- move to prev result
-            ["<C-j>"] = actions.move_selection_next, -- move to next result
+            ["<C-k>"] = actions.move_selection_previous,
+            ["<C-j>"] = actions.move_selection_next,
             ["<C-q>"] = actions.send_selected_to_qflist + custom_actions.open_trouble_qflist,
             ["<C-t>"] = trouble_telescope.open,
           },
         },
       },
+      pickers = {
+        oldfiles = {
+          cwd_only = true, -- chỉ hiển thị các file trong thư mục làm việc hiện tại
+        },
+      },
     })
+    
 
     telescope.load_extension("fzf")
 
@@ -46,6 +52,8 @@ return {
     keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
     keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
     keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
-    keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", { desc = "Live grep search" }) -- Thêm phím tắt mới
+    keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", { desc = "Live grep search" })
+    keymap.set("n", "<leader>fo", "<cmd>Telescope oldfiles<cr>", { desc = "Open recently opened files" })
+
   end,
 }
