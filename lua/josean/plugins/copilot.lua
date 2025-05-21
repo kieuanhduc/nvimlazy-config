79acd3c868eaa04526a34b9261f8867d2cd2b14c
AndrewDuc <andrewduc2722@gmail.com>
2025-05-21 04:46:07 +0000

    active github copilot

--- lua/josean/plugins/copilot.lua
+++ lua/josean/plugins/copilot.lua
@@ -1,37 +1,37 @@
 return {
-  -- {
-  --   "zbirenbaum/copilot.lua",
-  --   cmd = "Copilot",
-  --   build = ":Copilot auth",
-  --   event = "BufReadPost",
-  --   opts = {
-  --     suggestion = {
-  --       enabled = not vim.g.ai_cmp,
-  --       auto_trigger = true,
-  --       hide_during_completion = vim.g.ai_cmp,
-  --       keymap = {
-  --         accept = false, -- handled by nvim-cmp / blink.cmp
-  --         next = "<M-]>",
-  --         prev = "<M-[>",
-  --       },
-  --     },
-  --     panel = { enabled = false },
-  --     filetypes = {
-  --       markdown = true,
-  --       help = true,
-  --     },
-  --   },
-  -- },
-  -- {
-  --   "CopilotC-Nvim/CopilotChat.nvim",
-  --   branch = "canary",
-  --   dependencies = { "zbirenbaum/copilot.lua" },
-  --   config = function()
-  --     require("CopilotChat").setup()
-  --   end,
-  --   keys = {
-  --     { "<leader>cc", "<cmd>CopilotChat<cr>", desc = "Copilot Chat" }
-  --   }
-  -- }
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    build = ":Copilot auth",
+    event = "BufReadPost",
+    opts = {
+      suggestion = {
+        enabled = not vim.g.ai_cmp,
+        auto_trigger = true,
+        hide_during_completion = vim.g.ai_cmp,
+        keymap = {
+          accept = false, -- handled by nvim-cmp / blink.cmp
+          next = "<M-]>",
+          prev = "<M-[>",
+        },
+      },
+      panel = { enabled = false },
+      filetypes = {
+        markdown = true,
+        help = true,
+      },
+    },
+  },
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    branch = "canary",
+    dependencies = { "zbirenbaum/copilot.lua" },
+    config = function()
+      require("CopilotChat").setup()
+    end,
+    keys = {
+      { "<leader>cc", "<cmd>CopilotChat<cr>", desc = "Copilot Chat" }
+    }
+  }
 }
 
